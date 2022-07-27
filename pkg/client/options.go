1900aa004d8e4e96dfe1a9987b575e82a1fa4e58
pradeepnnv <pradeepnnv@gmail.com>
2022-07-27 13:19:14 -0500

    Commit code changes
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- pkg/client/options.go
+++ pkg/client/options.go
@@ -17,7 +17,7 @@ limitations under the License.
 package client
 
 import (
-	"github.com/kcp-dev/logicalcluster"
+	"github.com/kcp-dev/logicalcluster/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
