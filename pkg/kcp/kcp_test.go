2265f018a59dd9533492485990e8fa46c5d47bea
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2024-09-01 19:10:19 +0200

    UPSTREAM: <carry>: more kcp tests
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- pkg/kcp/kcp_test.go
+++ pkg/kcp/kcp_test.go
@@ -14,6 +14,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -47,6 +48,10 @@ var _ = Describe("NewClusterAwareClient", Ordered, func() {
 					return
 				}
 				fallthrough
+			case "/api/v1/namespaces/default/pods/foo", "/clusters/root/api/v1/namespaces/default/pods/foo":
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write([]byte(`{"kind":"Pod","apiVersion":"v1","metadata":{"name":"foo","namespace":"default","resourceVersion":"184126176"}}`))
 			default:
 				_, _ = w.Write([]byte(fmt.Sprintf("Not found %q", req.RequestURI)))
 				w.WriteHeader(http.StatusNotFound)
@@ -78,9 +83,13 @@ var _ = Describe("NewClusterAwareClient", Ordered, func() {
 			err = cl.List(ctx, pods)
 			Expect(err).NotTo(HaveOccurred())
 
+			pod := &corev1.Pod{}
+			err = cl.Get(ctx, types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
+			Expect(err).NotTo(HaveOccurred())
+
 			mu.Lock()
 			defer mu.Unlock()
-			Expect(paths).To(Equal([]string{"/api/v1", "/api/v1/pods"}))
+			Expect(paths).To(Equal([]string{"/api/v1", "/api/v1/pods", "/api/v1/namespaces/default/pods/foo"}))
 		})
 
 		It("should work with a cluster in the kontext", func(ctx context.Context) {
@@ -91,9 +100,13 @@ var _ = Describe("NewClusterAwareClient", Ordered, func() {
 			err = cl.List(kontext.WithCluster(ctx, "root"), pods)
 			Expect(err).NotTo(HaveOccurred())
 
+			pod := &corev1.Pod{}
+			err = cl.Get(kontext.WithCluster(ctx, "root"), types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
+			Expect(err).NotTo(HaveOccurred())
+
 			mu.Lock()
 			defer mu.Unlock()
-			Expect(paths).To(Equal([]string{"/clusters/root/api/v1", "/clusters/root/api/v1/pods"}))
+			Expect(paths).To(Equal([]string{"/clusters/root/api/v1", "/clusters/root/api/v1/pods", "/clusters/root/api/v1/namespaces/default/pods/foo"}))
 		})
 
 		It("should work with a wildcard cluster in the kontext", func(ctx context.Context) {
@@ -121,9 +134,15 @@ var _ = Describe("NewClusterAwareClient", Ordered, func() {
 			err = cl.List(ctx, pods)
 			Expect(err).NotTo(HaveOccurred())
 
+			pod := &unstructured.Unstructured{}
+			pod.SetAPIVersion("v1")
+			pod.SetKind("Pod")
+			err = cl.Get(ctx, types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
+			Expect(err).NotTo(HaveOccurred())
+
 			mu.Lock()
 			defer mu.Unlock()
-			Expect(paths).To(Equal([]string{"/api/v1", "/api/v1/pods"}))
+			Expect(paths).To(Equal([]string{"/api/v1", "/api/v1/pods", "/api/v1/namespaces/default/pods/foo"}))
 		})
 
 		It("should work with a cluster in the kontext", func(ctx context.Context) {
@@ -136,9 +155,15 @@ var _ = Describe("NewClusterAwareClient", Ordered, func() {
 			err = cl.List(kontext.WithCluster(ctx, "root"), pods)
 			Expect(err).NotTo(HaveOccurred())
 
+			pod := &unstructured.Unstructured{}
+			pod.SetAPIVersion("v1")
+			pod.SetKind("Pod")
+			err = cl.Get(kontext.WithCluster(ctx, "root"), types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
+			Expect(err).NotTo(HaveOccurred())
+
 			mu.Lock()
 			defer mu.Unlock()
-			Expect(paths).To(Equal([]string{"/clusters/root/api/v1", "/clusters/root/api/v1/pods"}))
+			Expect(paths).To(Equal([]string{"/clusters/root/api/v1", "/clusters/root/api/v1/pods", "/clusters/root/api/v1/namespaces/default/pods/foo"}))
 		})
 
 		It("should work with a wildcard cluster in the kontext", func(ctx context.Context) {
@@ -201,13 +226,18 @@ var _ = Describe("NewClusterAwareClient", Ordered, func() {
 
 var _ = Describe("NewClusterAwareCache", Ordered, func() {
 	var (
-		srv   *httptest.Server
-		mu    sync.Mutex
-		paths []string
-		cfg   *rest.Config
+		cancelCtx context.CancelFunc
+		srv       *httptest.Server
+		mu        sync.Mutex
+		paths     []string
+		cfg       *rest.Config
+		c         cache.Cache
 	)
 
 	BeforeAll(func() {
+		var ctx context.Context
+		ctx, cancelCtx = context.WithCancel(context.Background())
+
 		srv = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			mu.Lock()
 			pth := req.URL.Path
@@ -225,11 +255,16 @@ var _ = Describe("NewClusterAwareCache", Ordered, func() {
 			case req.URL.Path == "/clusters/*/api/v1/pods" && req.URL.Query().Get("watch") != "true":
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusOK)
-				_, _ = w.Write([]byte(`{"kind": "PodList","apiVersion": "v1","metadata": {"resourceVersion": "184126176"}, "items": [{"kind":"Pod","apiVersion":"v1","metadata":{"name":"foo","namespace":"default","resourceVersion":"184126176","annotations":{"kcp.io/cluster":"root"}}}]}`))
+				_, _ = w.Write([]byte(`{"kind": "PodList","apiVersion": "v1","metadata": {"resourceVersion": "184126176"}, "items": [
+					{"kind":"Pod","apiVersion":"v1","metadata":{"name":"foo","namespace":"default","resourceVersion":"184126176","annotations":{"kcp.io/cluster":"root"}}},
+					{"kind":"Pod","apiVersion":"v1","metadata":{"name":"foo","namespace":"default","resourceVersion":"184126093","annotations":{"kcp.io/cluster":"ws"}}}
+				]}`))
 			case req.URL.Path == "/clusters/*/api/v1/pods" && req.URL.Query().Get("watch") == "true":
 				w.Header().Set("Content-Type", "application/json")
 				w.Header().Set("Transfer-Encoding", "chunked")
 				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write([]byte(`{"type":"ADDED","object":{"kind":"Pod","apiVersion":"v1","metadata":{"name":"bar","namespace":"default","resourceVersion":"184126177","annotations":{"kcp.io/cluster":"root"}}}}`))
+				_, _ = w.Write([]byte(`{"type":"ADDED","object":{"kind":"Pod","apiVersion":"v1","metadata":{"name":"bar","namespace":"default","resourceVersion":"184126178","annotations":{"kcp.io/cluster":"ws"}}}}`))
 				if w, ok := w.(http.Flusher); ok {
 					w.Flush()
 				}
@@ -239,11 +274,25 @@ var _ = Describe("NewClusterAwareCache", Ordered, func() {
 				_, _ = w.Write([]byte(fmt.Sprintf("Not found %q", req.RequestURI)))
 			}
 		}))
+		go func() {
+			<-ctx.Done()
+			srv.Close()
+		}()
 
 		cfg = &rest.Config{
 			Host: srv.URL,
 		}
 		Expect(rest.SetKubernetesDefaults(cfg)).To(Succeed())
+
+		var err error
+		c, err = NewClusterAwareCache(cfg, cache.Options{})
+		Expect(err).NotTo(HaveOccurred())
+		go func() {
+			if err := c.Start(ctx); err != nil {
+				Expect(err).NotTo(HaveOccurred())
+			}
+		}()
+		c.WaitForCacheSync(ctx)
 	})
 
 	BeforeEach(func() {
@@ -253,25 +302,38 @@ var _ = Describe("NewClusterAwareCache", Ordered, func() {
 	})
 
 	AfterAll(func() {
-		srv.Close()
+		cancelCtx()
 	})
 
 	It("should always access wildcard clusters and serve other clusters from memory", func(ctx context.Context) {
-		c, err := NewClusterAwareCache(cfg, cache.Options{})
-		Expect(err).NotTo(HaveOccurred())
-		go func() {
-			if err := c.Start(ctx); err != nil {
-				Expect(err).NotTo(HaveOccurred())
-			}
-		}()
-		c.WaitForCacheSync(ctx)
-
 		pod := &corev1.Pod{}
-		err = c.Get(kontext.WithCluster(ctx, "root"), types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
+		err := c.Get(kontext.WithCluster(ctx, "root"), types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
 		Expect(err).NotTo(HaveOccurred())
 
 		mu.Lock()
 		defer mu.Unlock()
 		Expect(paths).To(Equal([]string{"/clusters/*/api/v1", "/clusters/*/api/v1/pods", "/clusters/*/api/v1/pods?watch=true"}))
+	})
+
+	It("should return only the pods from the requested cluster", func(ctx context.Context) {
+		pod := &corev1.Pod{}
+		err := c.Get(kontext.WithCluster(ctx, "root"), types.NamespacedName{Namespace: "default", Name: "foo"}, pod)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(pod.Annotations).To(HaveKeyWithValue("kcp.io/cluster", "root"))
+
+		pods := &corev1.PodList{}
+		err = c.List(kontext.WithCluster(ctx, "root"), pods)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(pods.Items).To(HaveLen(2))
+		Expect(pods.Items[0].Annotations).To(HaveKeyWithValue("kcp.io/cluster", "root"))
+		Expect(pods.Items[1].Annotations).To(HaveKeyWithValue("kcp.io/cluster", "root"))
+		Expect(sets.New(pods.Items[0].Name, pods.Items[1].Name)).To(Equal(sets.New("foo", "bar")))
+	})
+
+	It("should return all pods from all clusters without cluster in context", func(ctx context.Context) {
+		pods := &corev1.PodList{}
+		err := c.List(ctx, pods)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(pods.Items).To(HaveLen(4))
 	})
 })
